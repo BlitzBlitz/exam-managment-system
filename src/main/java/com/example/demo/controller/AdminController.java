d264d7e6c6c370a315c78a44974ff8b258ffd699
BlitzBlitz <klementbinaj@gmail.com>
2022-07-18 11:44:33 +0200

    display teachers done

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Teacher;
 import com.example.demo.entity.TableData;
+import com.example.demo.entity.TeacherRepository;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
@@ -11,7 +12,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class AdminController {
     @FXML
@@ -27,25 +31,24 @@ public class AdminController {
         loc.setText("/home/teachers");
         category.setText("Teachers");
         ObservableList<TableData> teachersData = FXCollections.observableArrayList();
-        teachersData.add(new TableData(new Teacher(1,"1","1","1","1","1")));
-
-        TableColumn<TableData, String> column = (TableColumn<TableData, String>)infoTable.getColumns().get(1);
+        infoTable.setPlaceholder(new Label("There are no teacher registered"));
+        try{
+            ArrayList<Teacher> teachers = TeacherRepository.getAllTeachers();
+            teachersData = FXCollections.observableArrayList(
+                    teachers.stream().map(teacher -> new TableData(teacher)).toArray(TableData[]::new));
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+            //TODO pop-up error
+        }
         infoTable.setItems(teachersData);
     }
 
     public void displayStudents(ActionEvent actionEvent) {
         loc.setText("/home/students");
         category.setText("Students");
+        infoTable.setPlaceholder(new Label("There are no students registered"));
+
     }
 
-    class GetTeachers extends Task{
-        @Override
-        protected Object call() throws Exception {
-            ArrayList<TableData> teachers = new ArrayList<>();
-            teachers.add(new TableData(new Teacher(1,"1","1","1","1","1")));
-            return FXCollections.observableArrayList(
-                    teachers
-            );
-        }
-    }
+
 }
